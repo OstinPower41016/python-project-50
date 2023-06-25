9f012cb47ac0671abc55a2657f4785ea50f8fb2d
Nikolay Larionov <ostinpower.1101@gmail.com>
2023-06-25 22:30:08 +0300

    [N/A] initial

--- genDiff/args_parser.py
+++ genDiff/args_parser.py
@@ -6,6 +6,9 @@ def get_args():
         description='Compares two configuration files and shows a difference.')
     gendiff.add_argument('first_file')
     gendiff.add_argument('second_file')
+    gendiff.add_argument('-f', '--format', help='set format of output',
+                         default='stylish',
+                         choices=['stylish', 'plain', 'json'])
 
     args = gendiff.parse_args()
     return args
